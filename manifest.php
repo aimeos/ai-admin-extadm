2acf9c796bf151ec4d9189dd4790b65c068acf2e
Aimeos <aimeos@aimeos.org>
2016-05-03 23:53:47 +0200

    Added admin i18n directories

--- manifest.php
+++ manifest.php
@@ -11,6 +11,8 @@ return array(
 		'controller/jobs/src',
 	),
 	'i18n' => array(
+		'admin' => 'admin/i18n',
+		'admin/ext' => 'admin/i18n/ext',
 		'controller/extjs' => 'controller/extjs/i18n',
 		'controller/jobs' => 'controller/jobs/i18n',
 	),
